ea06433221d362826db515bbbeab39f0019a7ba7
Anel CC <anel.calvo.cervantes@gmail.com>
2020-03-28 21:41:22 -0400

    [Anel] Use Data class

--- app/src/main/java/com/anelcc/coronavirustrack/ui/countrydetail/CountryDetail.kt
+++ app/src/main/java/com/anelcc/coronavirustrack/ui/countrydetail/CountryDetail.kt
@@ -1,35 +1,13 @@
 package com.anelcc.coronavirustrack.ui.countrydetail
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-class CountryDetail {
-    @SerializedName("country")
-    @Expose
-    var country: String? = null
-    @SerializedName("cases")
-    @Expose
-    var cases: Int? = null
-    @SerializedName("todayCases")
-    @Expose
-    var todayCases: Int? = null
-    @SerializedName("deaths")
-    @Expose
-    var deaths: Int? = null
-    @SerializedName("todayDeaths")
-    @Expose
-    var todayDeaths: Int? = null
-    @SerializedName("recovered")
-    @Expose
-    var recovered: Int? = null
-    @SerializedName("active")
-    @Expose
-    var active: Int? = null
-    @SerializedName("critical")
-    @Expose
-    var critical: Int? = null
-    @SerializedName("casesPerOneMillion")
-    @Expose
+data class CountryDetail (
+    var country: String? = null,
+    var cases: Int? = null,
+    var todayCases: Int? = null,
+    var deaths: Int? = null,
+    var todayDeaths: Int? = null,
+    var recovered: Int? = null,
+    var active: Int? = null,
+    var critical: Int? = null,
     var casesPerOneMillion: Double? = null
-
-}
+)

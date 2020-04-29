09dda55bd2bbf49f296a39a49383c71a2093b70e
Anel CC <anel.calvo.cervantes@gmail.com>
2020-04-29 19:52:13 -0400

    [Anel][Covid19-06] Add image in Country details

--- app/src/main/java/com/anelcc/coronavirustrack/ui/countrydetail/DetailConstant.kt
+++ app/src/main/java/com/anelcc/coronavirustrack/ui/countrydetail/DetailConstant.kt
@@ -4,4 +4,5 @@ package com.anelcc.coronavirustrack.ui.countrydetail
 object DetailConstant{
     const val EMPTY_STRING: String = ""
     const val COUNTRY_KEY: String = "COUNTRY_KEY"
+    const val COUNTRY_FLAG: String = "COUNTRY_FLAG"
 }

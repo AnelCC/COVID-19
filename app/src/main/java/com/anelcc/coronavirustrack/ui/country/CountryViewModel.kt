1256a9dc70bb2b097685518a2fc4a8b05d716755
Anel CC <anel.calvo.cervantes@gmail.com>
2020-05-08 20:02:47 -0400

    [Anel][COVID_08-Search] 1. Update viewModel

--- app/src/main/java/com/anelcc/coronavirustrack/ui/country/CountryViewModel.kt
+++ app/src/main/java/com/anelcc/coronavirustrack/ui/country/CountryViewModel.kt
@@ -1,6 +1,25 @@
 package com.anelcc.coronavirustrack.ui.country
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
 import androidx.lifecycle.ViewModel
+import com.anelcc.coronavirustrack.ui.countrydetail.DetailConstant
+import com.anelcc.coronavirustrack.utils.NumberFormated
 
-class CountryViewModel(val country: String, val cases: String, val flag: String): ViewModel() {
+class CountryViewModel(val country: String, val cases: String, val flag: String): ViewModel(), Observable {
+    private val registry = PropertyChangeRegistry()
+
+    @Bindable
+    public fun getCasesAmount(): String {
+        return if (cases == null) DetailConstant.EMPTY_STRING else NumberFormated.numberFormat(cases.toInt()).toString()
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        registry.add(callback);
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        registry.remove(callback);
+    }
 }
